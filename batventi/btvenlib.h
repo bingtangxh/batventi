84dab2e008dc5d7839768d5c2c34e013c5a624e8
冰糖XH <76904365+bingtangxh@users.noreply.github.com>
2026-02-17 12:45:44 +0800

    Add some partial code in plugin_launcher.h

--- batventi/btvenlib.h
+++ batventi/btvenlib.h
@@ -35,6 +35,11 @@
 // User32 用于 ExitWindowsEx, ShowWindow, GetForegroundWindow
 #pragma comment(lib, "Kernel32.lib")
 
+#pragma comment(linker,"\"/manifestdependency:type='win32' \
+name='Microsoft.Windows.Common-Controls' version='6.0.0.0' \
+processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
+
 LPWSTR _MultiByteToWideChar(const UINT CodePage, const char *source);
 errno_t __cdecl _mbstowcs_s(size_t * const convertedCharsNum, LPWSTR * const dest, const char *source);
 const char *specifyParameter(const char *switchN, const char *currPara, const char *nextPara, int *errCode);
--- batventi/plugin_launcher.h
+++ batventi/plugin_launcher.h
@@ -2,8 +2,8 @@
 #include <Windows.h>
 #include "btvenlib.h"
 
-
-
+_Bool getModuleFileName(wchar_t *);
+_Bool getModuleDirectory(wchar_t *);
 
 
 
@@ -18,8 +18,40 @@
 
 
 int plugin_launcher(int argc, char **argv) {
-
-
+	// 暂时先硬编码一个静态长度吧
+    wchar_t modulePath[MAX_PATH];
+	if(getModuleDirectory(modulePath)) {
+		putsHyphen2("Error: Failed to get the module directory.");
+		return 1;
+    }
+    wcscat_s(modulePath,MAX_PATH,L"plugin\\");
+    MultiByteToWideChar(CP_ACP,0,argv[1],-1,modulePath+wcslen(modulePath),MAX_PATH);
+    wcscat_s(modulePath,MAX_PATH,L".exe");
+	_putws(modulePath);
 	putsHyphen2("This feature is not finished yet.\nHow about we explore the area ahead of us later?");
+	return 0;
+}
+
+_Bool getModuleFileName(wchar_t *buffer) {
+	HMODULE hModule = GetModuleHandleW(NULL);
+	if (hModule == NULL) {
+		return 1;
+	}
+	DWORD result = GetModuleFileNameW(hModule, buffer, MAX_PATH);
+	if (result == 0 || result == MAX_PATH) {
+		return 1;
+	}
+	return 0;
+}
+
+_Bool getModuleDirectory(wchar_t *buffer) {
+	if (getModuleFileName(buffer)) {
+		return 1;
+	}
+	wchar_t *lastBackslash = wcsrchr(buffer, L'\\');
+	if (lastBackslash == NULL) {
+		return 1;
+	}
+	*(lastBackslash + 1) = L'\0';
 	return 0;
 }
